0152f375ce9c612195eda1353d2e60e1e8b1bfd7
David Cantoran <davidcantoran36@gmail.com>
2019-12-07 02:01:36 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -11,7 +11,7 @@ require 'pry'
 def get_the_min(groceries)
   #code your solution here!
   groceries.each do |key, value|
-    value.sort.min
+    return value.sort.min
     # binding.pry
   end 
 end
